215ecf79c2df634ba3b14f0041a4b19331e4214d
Tim Leerhoff <tleerhof@web.de>
2016-03-12 18:48:09 +0100

    Added license-headers.

--- src/mtudetect.h
+++ src/mtudetect.h
@@ -1,9 +1,25 @@
-/**
- * @file ping.h
+/*
+ * Copyright (C) 2016 by Tim Leerhoff <tleerhof@web.de>
  *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+
+/**
+ * @file mtudetect.h
  * @brief Functions and return values for MTU detection.
- * 
- * @author Tim Leerhoff <tleerhof@web.de>, (C) 2016
  */
 #ifndef __MTUDETECT_H_
 #define __MTUDETECT_H_
