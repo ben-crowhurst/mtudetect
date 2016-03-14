728b36d7fd48310125bf70dba3d2bb7ae2863cf1
Tim Leerhoff <tleerhof@web.de>
2016-03-14 17:05:20 +0100

    Added deamon-code to main. Disabled rest.

--- src/main.c
+++ src/main.c
@@ -24,7 +24,62 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <syslog.h>
+
+#include <sys/types.h>		// for umask
+#include <sys/stat.h>
+#include <sys/resource.h> 	// for rlimit
+
+#include <fcntl.h>		// for open
+#include <unistd.h>		// for fork, close
+
+#include <errno.h>
+
+#include <sys/socket.h>         // socket()
+#include <arpa/inet.h>          // Funktionen wie inet_addr()
+#include <netinet/in.h>         // IP Protokolle, sockaddr_in Struktur und Funktionen wie htons()
+
 #include "mtudetect.h"
+
+/**
+ * Prints the last error and exits the application.
+ */
+void quitError(const char* mesg)
+{
+//  perror(mesg);
+  syslog(LOG_ERR, "%s (%d)", mesg, errno); 
+  exit(1);
+}
+
+void closeAllFiledescriptors()
+{
+  struct rlimit rl;
+  if(getrlimit(RLIMIT_NOFILE, &rl) < 0)
+  {
+    quitError("getrlimit");
+  }
+  if(rl.rlim_max == RLIM_INFINITY)
+  {
+    rl.rlim_max = 1024;
+  }
+
+  int fd;
+  for(fd=0;fd<rl.rlim_max;fd++)
+  {
+    close(fd);
+  }
+}
+
+void redirectStdInOutErr()
+{
+  int fd0 = open("/dev/null", O_RDWR);
+  int fd1 = dup(0);
+  int fd2 = dup(0);
+  if(fd0 != 0 || fd1 != 1 || fd2 != 2 )
+  {
+    quitError("redirectStdInOutErr");
+  }
+}
 
 /**
  * The main programm
@@ -39,7 +94,75 @@ int main( int argc, char *argv[] )
     fprintf(stderr, "Usage: mtudetect <ip> <mtu>\n");
     return 1;
   }
+
+  umask(0);
+  closeAllFiledescriptors();
+  redirectStdInOutErr();
+
+  int sock = socket( AF_INET, SOCK_RAW, IPPROTO_ICMP );
+  if(sock == -1 )
+  {
+    quitError("socket");
+  }
+
+  int one = 1;
+  if(setsockopt( sock, IPPROTO_IP, IP_HDRINCL, &one, sizeof(one) ) != 0 )
+  {
+    quitError("setsocketopt");
+  }
+
+  // Become a normal user
+  if(seteuid(1) < 0)
+  {
+    quitError("seteuid");
+  }
+
+  // fork parent
+  pid_t pid = fork();
+  if(pid < 0)
+  {
+    // error
+    quitError("fork");
+  }
+  else
+  if(pid == 0)
+  {
+    // child
+  }
+  else
+  {
+    // parent
+    close(sock);
+    exit(0);
+  }
+
+
+  // Create a new session and process group...
+  setsid();
+
+
+  // Change directory to root not to block other filesystems
+  if(chdir("/") < 0)
+  {
+    quitError("chdir");
+  }
+
+
+  openlog(argv[0], LOG_CONS, LOG_DAEMON);
+  syslog(LOG_INFO, "Initialized daemon");
+
+  if(close(sock) < 0)
+  {
+    quitError("close");
+  }
+
   int result = checkMTU(argv[1], atoi(argv[2]));
   fprintf(stdout, "Result: %s(%d)\n", getReturnValueText(result), result);
-  return result;
+
+
+  //sleep(1000);
+
+  syslog(LOG_INFO, "Shutdown daemon");
+  closelog();
+  exit(result);
 }
